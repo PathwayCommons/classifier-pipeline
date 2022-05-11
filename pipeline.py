7bdfb516c73c2d8e779fc9ac02805157a7e2b9bd
Jeffrey Wong <jvwong@gmail.com>
2022-05-10 21:37:00 -0400

    Functioning classifier pipeline

--- pipeline.py
+++ pipeline.py
@@ -2,8 +2,11 @@ import csv
 import itertools
 import sys
 from ncbiutils.ncbiutils import PubMedFetch
+from pathway_abstract_classifier.pathway_abstract_classifier import Classifier
+from loguru import logger
+import time
 # import getopt
-# from memory_profiler import profile
+from memory_profiler import profile
 # import argparse
 
 # parser = argparse.ArgumentParser()
@@ -16,6 +19,16 @@ def as_pipeline(steps):
         generator = step(generator)
     return generator
 
+####################################################
+#                  Utils
+####################################################
+
+list2generator = lambda x : (n for n in x)
+
+def counter(items):
+    item_list = list(items)
+    print(f'Number of items: {len(item_list)}')
+    return list2generator(item_list)
 
 ####################################################
 #                  Extract
@@ -44,6 +57,26 @@ def limit_filter(number):
         yield from itertools.islice(items, number)
     return _limit
 
+# @profile
+def classification_transformer(**opts):
+    "Filter the chunks of citations based on text content"
+    classifier = Classifier(**opts)
+    def _classification_transformer(chunks):
+        for chunk in chunks:
+            error, citations, ids = chunk
+            if error is not None:
+                print(f'Error retrieving ids: {ids}')
+                continue
+            else:
+                logger.info('Got a chunk of {n} uids to classify', n=len(ids))
+                documents = [c.dict() for c in citations]
+                start = time.time()
+                predictions = classifier.predict(documents)
+                end = time.time()
+                logger.info('Finished classification in {elapsed} seconds', elapsed=(end - start))
+                yield from predictions
+    return _classification_transformer
+
 ####################################################
 #                  Transform
 ####################################################
@@ -55,11 +88,13 @@ def list_transformer(field):
             yield item[field]
     return _list_transformer
 
-def pubmed_citation_transformer(items):
+def pubmed_citation_transformer(**opts):
     "Retrieve the PubMed record"
-    pmf = PubMedFetch()
-    uids = list(items)
-    return pmf.get_citations(uids)
+    pmf = PubMedFetch(**opts)
+    def _pubmed_citation_transformer(items):
+        uids = list(items)
+        return pmf.get_citations(uids)
+    return _pubmed_citation_transformer
 
 ####################################################
 #                  Load
@@ -69,20 +104,34 @@ def print_loader(items):
     for item in items:
         print(item)
 
-def count_loader(items):
-    print(f'Number of items: {len(list(items))}')
+def pumbmed_print_loader(chunks):
+    for chunk in chunks:
+        error, citations, ids = chunk
+        if error is not None:
+            print(f'Error retrieving ids: {ids}')
+            continue
+        else:
+            documents = [c.dict() for c in citations]
+            for d in documents:
+                print(f'Retrieved doc pmid: {d["pmid"]} -- title: {d["title"]}')
 
+def prediction_print_loader(predictions):
+    for prediction in predictions:
+        document, classification, probability = prediction
+        print(f'pmid: {document["pmid"]} -- classification: {classification} -- probability: {probability}')
 
 if __name__ == '__main__':
     # args = parser.parse_args()
     # print(args.echo)
 
-    by_evidences = lambda x: int(x['indra_evidences_entity_constraint']) > 100
+    by_evidences = lambda x: int(x['indra_statements_entity_constraint']) > 1
 
     pipeline = as_pipeline([
         csv2dict_reader(sys.stdin),
         filter(by_evidences),
         list_transformer('pmid'),
-        pubmed_citation_transformer,
-        print_loader
+        # counter
+        pubmed_citation_transformer(retmax = 1000),
+        classification_transformer(),
+        prediction_print_loader
     ])
