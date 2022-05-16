aba67661c6c437346af48d1327a289cf4181ce4b
Jeffrey Wong <jvwong@gmail.com>
2022-05-16 15:35:15 -0400

    Split up functions into modules, tests.

--- classifier_pipeline/classifier_pipeline.py
+++ classifier_pipeline/classifier_pipeline.py
@@ -1,95 +1,14 @@
-import csv
-from typing import Dict, List, Callable, Any, Generator, IO
-import sys
+from typing import Callable, Any, Generator, List
 from ncbiutils.ncbiutils import PubMedFetch, PubMedDownload
 from ncbiutils.pubmedxmlparser import Citation
 from pathway_abstract_classifier.pathway_abstract_classifier import Classifier, Prediction
 from loguru import logger
 import time
-import argparse
-
-
-####################################################
-#            Command line args
-####################################################
-retmax_limit = 10000
-default_threshold = 0.5
-parser = argparse.ArgumentParser()
-parser.add_argument('--retmax', nargs='?', type=int, default=str(retmax_limit))
-parser.add_argument('--threshold', nargs='?', type=float, default=str(default_threshold))
-parser.add_argument('--type', nargs='?', type=str, default='fetch')
-parser.add_argument('--idcolumn', nargs='?', type=str, default='pmid')
-
-
-def get_opts() -> Dict[str, Any]:
-    args = parser.parse_args()
-    opts = {'retmax': args.retmax, 'threshold': args.threshold, 'type': args.type, 'idcolumn': args.idcolumn}
-
-    if opts['retmax'] < 0:
-        raise ValueError('retmax must be non-negative')
-    elif opts['retmax'] > retmax_limit:
-        opts['retmax'] = retmax_limit
-
-    if opts['threshold'] < 0 or opts['threshold'] > 1:
-        raise ValueError('threshold must be on [0, 1]')
-
-    return opts
-
-
-####################################################
-#                 Pipeline
-####################################################
-
-
-def as_pipeline(steps: List[Callable[[Any], Any]]) -> Callable[[Any], Any]:
-    generator = steps.pop(0)
-    for step in steps:
-        generator = step(generator)
-    return generator
-
-
-####################################################
-#                  Extract
-####################################################
-
-
-def csv2dict_reader(stream: IO) -> Callable[[None], Generator[Dict[str, Any], None, None]]:
-    """Return a reader that streams csv to dicts"""
-
-    def _csv2dict_reader():
-        reader = csv.DictReader(stream)
-        yield from reader
-
-    return _csv2dict_reader()
 
 
 ####################################################
 #                  Filter
 ####################################################
-
-
-def filter(predicate: Callable[[Any], Any]) -> Callable[[Generator[Any, None, None]], Generator[Any, None, None]]:
-    def _filter(items):
-        for item in items:
-            if predicate(item):
-                yield item
-
-    return _filter
-
-
-def limit_filter(limit: int) -> Callable[[Generator[Any, None, None]], Generator[Any, None, None]]:
-    counter = 0
-
-    def _limit(items):
-        nonlocal counter
-        for item in items:
-            if counter == limit:
-                break
-            else:
-                counter += 1
-                yield item
-
-    return _limit
 
 
 def citation_pubtype_filter(citations: Generator[Citation, None, None]) -> Generator[Citation, None, None]:
@@ -124,40 +43,9 @@ def citation_pubtype_filter(citations: Generator[Citation, None, None]) -> Gener
 ####################################################
 
 
-def chunker(size: int) -> Callable[[Generator[Any, None, None]], Generator[Any, None, None]]:
-    """Aggregate items into chunks of a given size"""
-
-    def _chunker(items):
-        chunk = []
-        while True:
-            try:
-                if len(chunk) == size:
-                    yield chunk
-                    chunk = []
-                else:
-                    item = next(items)
-                    chunk.append(item)
-            except StopIteration:
-                if chunk:
-                    yield chunk
-                break
-
-    return _chunker
-
-
-def list_transformer(field: str) -> Callable[[Generator[Any, None, None]], Generator[Any, None, None]]:
-    """Create a list from a field"""
-
-    def _list_transformer(items):
-        for item in items:
-            yield item[field]
-
-    return _list_transformer
-
-
 def classification_transformer(
     **opts,
-) -> Callable[[Generator[Citation, None, None]], Generator[Prediction, None, None]]:
+) -> Callable[[Generator[List[Citation], None, None]], Generator[Prediction, None, None]]:
     """Filter the chunks of articles based on text content"""
     classifier = Classifier(**opts)
 
@@ -178,7 +66,7 @@ def classification_transformer(
 
 def pubmed_transformer(
     type: str = 'fetch', **opts
-) -> Callable[[Generator[Any, None, None]], Generator[Citation, None, None]]:
+) -> Callable[[Generator[str, None, None]], Generator[Citation, None, None]]:
     """Retrieve the PubMed records"""
     pmt = PubMedDownload() if type == 'download' else PubMedFetch(**opts)
 
@@ -194,54 +82,3 @@ def pubmed_transformer(
                 yield from citations
 
     return _pubmed_fetch_transformer
-
-
-####################################################
-#                  Load
-####################################################
-
-
-def print_loader(items):
-    for item in items:
-        logger.info(len(item))
-
-
-def prediction_print_loader(predictions):
-    tcount = 0
-    pcount = 0
-    for p in predictions:
-        tcount += 1
-        if p.classification == 1:
-            pcount += 1
-            logger.info(
-                'Positive {n} out of {t} analyzed ({rate}%) -- pmid: {pmid}; prob: {prob}',
-                n=pcount,
-                t=tcount,
-                rate=pcount / tcount,
-                pmid=p.document['pmid'],
-                prob=p.probability,
-            )
-    logger.info('Analyzed {tcount} articles', tcount=tcount)
-    logger.info('Identified {pcount} positives', pcount=pcount)
-
-
-####################################################
-#                 __main__
-####################################################
-
-if __name__ == '__main__':
-    opts = get_opts()
-    print(opts)
-
-    pipeline = as_pipeline(
-        [
-            csv2dict_reader(sys.stdin),
-            list_transformer(opts['idcolumn']),
-            pubmed_transformer(type=opts['type']),
-            citation_pubtype_filter,
-            limit_filter(100000),
-            chunker(1000),
-            classification_transformer(threshold=opts['threshold']),
-            prediction_print_loader,
-        ]
-    )
--- tests/test_classifier_pipeline.py
+++ tests/test_classifier_pipeline.py
@@ -1,98 +1,62 @@
 import pytest
-from classifier_pipeline.classifier_pipeline import (
-    csv2dict_reader,
-    filter,
-    limit_filter,
-    list_transformer,
-    chunker,
-    citation_pubtype_filter,
-)
+from classifier_pipeline.classifier_pipeline import citation_pubtype_filter, classification_transformer, pubmed_transformer
 from ncbiutils.pubmedxmlparser import Citation
+from pathway_abstract_classifier.pathway_abstract_classifier import Prediction
 
-
-@pytest.fixture
-def numeric_items():
-    items = range(10)
-    return (item for item in items)
-
-
-@pytest.fixture
-def dict_items():
-    items = [{'field1': 1, 'field2': 2}, {'field1': 3, 'field2': 4}]
-    return (item for item in items)
-
-
-@pytest.fixture
-def updatefiles_stream(shared_datadir):
-    return (shared_datadir / 'updatefiles.csv').open()
-
+uids = ('1', '2', '3')
+citations = (
+    Citation(pmid='1', title='title1', abstract='', journal={}, publication_type_list=['D016428']),
+    Citation(pmid='2', title='title2', abstract='', journal={}, publication_type_list=['D016454', 'D016428']),
+    Citation(pmid='3', title='title3', abstract='', journal={}, publication_type_list=['D055823'])
+)
 
 @pytest.fixture
 def citation_items():
-    c_article = Citation(pmid='pmid1', title='title1', journal={}, publication_type_list=['D016428'])
-    c_review = Citation(pmid='pmid2', title='title2', journal={}, publication_type_list=['D016454', 'D016428'])
-    c_cookbook = Citation(pmid='pmid3', title='title3', journal={}, publication_type_list=['D055823'])
-    return (c for c in [c_article, c_review, c_cookbook])
+    return (c for c in citations)
 
+@pytest.fixture
+def citation_chunks():
+    yield [citations]
 
-####################################################
-#                  Extract
-####################################################
+@pytest.fixture
+def prediction_items():
+    c_as_dicts = [c.dict() for c in citations]
+    return (Prediction(document = c, classification = 0, probability = 0.5) for c in c_as_dicts)
 
+@pytest.fixture
+def uid_items():
+    return (c for c in uids)
 
-def test_csv2dict_reader_generates_correct_keys(updatefiles_stream):
-    items = csv2dict_reader(updatefiles_stream)
-    item = next(items)
-    assert item is not None
-    assert isinstance(item, dict)
-    assert 'filename' in item
-    assert 'type' in item
-
+@pytest.fixture
+def citations_chunks():
+    error = None
+    yield [(error, citations, uids)]
 
 ####################################################
 #                  Filter
 ####################################################
 
 
-def test_filter(numeric_items):
-    predicate = lambda x: x < 5
-    items = filter(predicate)(numeric_items)
-    results = list(items)
-    assert len(results) == 5
-
-
-def test_limit_filter(numeric_items):
-    limit = 3
-    items = limit_filter(limit)(numeric_items)
-    results = list(items)
-    assert len(results) == limit
-
-
-def test_pubtype_filter(citation_items):
+def test_citation_pubtype_filter(citation_items):
     citations = list(citation_pubtype_filter(citation_items))
     assert len(citations) == 1
-    assert citations[0].pmid == 'pmid1'
-
+    assert citations[0].pmid == uids[0]
 
 ####################################################
 #                  Transform
 ####################################################
 
 
-def test_list_transformer(dict_items):
-    field = 'field1'
-    items = list_transformer(field)(dict_items)
-    results = list(items)
-    assert isinstance(results, list)
-    assert 1 in results
-    assert 3 in results
+def test_classification_transformer(mocker, citation_chunks, prediction_items):
+    mocker.patch('classifier_pipeline.classifier_pipeline.Classifier.predict', return_value=prediction_items)
+    predictions = classification_transformer()(citation_chunks)
+    p_list = list(predictions)
+    assert p_list is not None
+    assert len(p_list) == 3
 
 
-def test_chunker(numeric_items):
-    size = 3
-    items = chunker(size)(numeric_items)
-    results = list(items)
-    assert len(results[0]) == 3
-    assert len(results[1]) == 3
-    assert len(results[2]) == 3
-    assert len(results[3]) == 1
+def test_pubmed_transformer(mocker, uid_items, citations_chunks):
+    mock_response = ()
+    mocker.patch('classifier_pipeline.classifier_pipeline.PubMedFetch.get_citations', return_value=citations_chunks)
+    citations = list(pubmed_transformer()(uid_items))
+    assert len(citations) == 3
