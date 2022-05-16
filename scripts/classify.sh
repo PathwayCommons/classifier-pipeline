f38630f32a00f5a75f3e1728ddf54678e9317f28
Jeffrey Wong <jvwong@gmail.com>
2022-05-16 17:01:50 -0400

    updating the names of the scripts and data

--- scripts/classify.sh
+++ scripts/classify.sh
@@ -4,4 +4,4 @@
 nohup python pipeline.py --type fetch --idcolumn pmid < ./data/pmid_indra_counts_hasStatement.csv > pmid_indra_counts_hasStatement.out 2>&1 &
 
 ### Update files
-# nohup python pipeline.py --type fetch --idcolumn pmid < ./data/pmid_indra_counts_hasStatement.csv > pmid_indra_counts_hasStatement.out 2>&1 &
+# nohup python pipeline.py --type download --idcolumn filename < ./data/ftp_updatefiles.csv > ftp_updatefiles.out 2>&1 &
