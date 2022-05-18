e7da3da1239c257864e0f1c1f35ba8e54a4c7ec1
Jeffrey Wong <jvwong@gmail.com>
2022-05-17 21:12:18 -0400

    Work in progress

--- scripts/classify.sh
+++ scripts/classify.sh
@@ -3,26 +3,27 @@
 # This is a helper script to run a python script
 
 echo "--"
-echo "Starting $JOB_NAME"
+echo "Starting ${JOB_NAME} within environment ${CONDA_ENV}"
 date
 
 WORKING_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # stop the old screen session
 echo "Quitting old screen session..."
-# screen -S $JOB_NAME -X -p 0 stuff ^C && echo "Sent ^C" || echo "No screen session to ^C"
-# screen -S $JOB_NAME -X quit && echo "Quit old screen session" || echo "No screen session to stop"
+screen -S $JOB_NAME -X -p 0 stuff ^C && echo "Sent ^C" || echo "No screen session to ^C"
+screen -S $JOB_NAME -X quit && echo "Quit old screen session" || echo "No screen session to stop"
 
-# swap out old workspace with new one
+# activate the environment
+
+
 # start the server in a screen session
 echo "Starting new screen session..."
-# screen -d -m -S $JOB_NAME bash -c "npm run ${START_SCRIPT:-start} 2>&1 | tee ~/$JOB_NAME.screen.log"
+screen -d -m -S $JOB_NAME bash -c "python pipeline.py --type ${ARG_TYPE} --idcolumn ${ARG_IDCOLUMN} --table ${ARG_TABLE} --threshold ${ARG_THRESHOLD} < ${DATA_PATH}  2>&1 | tee ${LOG_PATH}"
 echo "New screen session started"
-
 echo "CI script complete"
 
 
-# python pipeline.py --type fetch --idcolumn pmid --table indra --threshold 0.990 < ./data/pmid_indra_counts_hasStatement.csv > pmid_indra_counts_hasStatement.out 2>&1
+# python pipeline.py --type ${ARG_TYPE} --idcolumn ${ARG_IDCOLUMN} --table ${ARG_TABLE} --threshold ${ARG_THRESHOLD} < ${DATA_PATH} > ${LOG_PATH}
 
 ## Ben's INDRA output
 # nohup python pipeline.py --type fetch --idcolumn pmid --table indra --threshold 0.990 < ./data/pmid_indra_counts_hasStatement.csv > pmid_indra_counts_hasStatement.out 2>&1 &
--- scripts/indra.sh
+++ scripts/indra.sh
@@ -4,12 +4,20 @@
 
 WORKING_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+export CONDA_ENV="flow"
 export JOB_NAME="indra-classification"
-export JOB_NAME="indra-classification"
-export JOB_NAME="indra-classification"
-export JOB_NAME="indra-classification"
-export JOB_NAME="indra-classification"
-export JOB_NAME="indra-classification"
-export JOB_NAME="indra-classification"
+
+# pipeline.py arguments
+export ARG_TYPE="fetch"
+export ARG_IDCOLUMN="pmid"
+export ARG_TABLE="indra"
+export ARG_THRESHOLD="0.990"
+
+# data file
+DATA_DIR="data"
+DATA_FILE="pmid_indra_counts_hasStatement_lite.csv"
+LOG_FILE="pmid_indra_counts_hasStatement.log"
+export DATA_PATH="${WORKING_DIR}/${DATA_DIR}/${DATA_FILE}"
+export LOG_PATH="${WORKING_DIR}/${DATA_DIR}/${LOG_FILE}"
 
 ./classify.sh
