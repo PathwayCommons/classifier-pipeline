e5255491a5c315020ae5e8f381f7d7cfcafda98b
Jeffrey Wong <jvwong@gmail.com>
2022-05-30 12:45:18 -0400

    Dump when available

--- scripts/cron/rethinkdb_dump.sh
+++ scripts/cron/rethinkdb_dump.sh
@@ -7,12 +7,18 @@ set -e
 # Environment variables
 WORKING_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 DOCKER="/usr/bin/docker"
+COMPOSE="/usr/local/bin/docker-compose --file ${WORKING_DIR}/../../docker-compose.yml"
 
 DATE=$(date +"%Y_%m_%d_%I_%M_%p")
 DB_DUMP_FILE="rethinkdb_dump_${DATE}.tar.gz"
 DB_NAME=classifier
 DB_WORKDIR=/data
 
-echo "Dump the database ${DB_NAME} to ${WORKING_DIR}"
-${DOCKER} exec -t db /bin/bash -c "rethinkdb dump --connect localhost:28015 --export ${DB_NAME} --file ${DB_DUMP_FILE}"
-${DOCKER} cp db:${DB_WORKDIR}/${DB_DUMP_FILE} ${WORKING_DIR}
+if [[ -z $(${COMPOSE} ps -aq) ]]; then
+    echo "Dump the database ${DB_NAME} to ${WORKING_DIR}"
+    ${DOCKER} exec -t db /bin/bash -c "rethinkdb dump --connect localhost:28015 --export ${DB_NAME} --file ${DB_DUMP_FILE}"
+    ${DOCKER} cp db:${DB_WORKDIR}/${DB_DUMP_FILE} ${WORKING_DIR}
+else
+    echo "No database available"
+fi
+
